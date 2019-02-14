4117b2f0e0c21ba22ebe0beb46bda14f15258e72
Gilles <gabbygilles1@gmail.com>
2019-02-14 14:07:59 +0000

    working on noteform

---
+++ notes/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from .models import Note
+
+
+class NoteForm(forms.ModelForm):
+
+    class Meta:
+        model = Note
+        fields = ('title', 'body', 'status', 'date')
+        labels = {
+            'title': 'Note Title',
+            'body': 'Message'
+        }
+
+    def __int__(self, *args, **kwargs):
+        super(NoteForm, self).__init__(*args, **kwargs)
+        self.fields['status'].empty_label = "Select"
--- notes/views.py
+++ notes/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, HttpResponse
+from .forms import NoteForm
 
 # Create your views here.
 
 
 def create(request):
-    return render(request, "notes/base.html")
+    form = NoteForm()
+    return render(request, "notes/index.html", {'form': form})
 
 
 def update(request, id):
